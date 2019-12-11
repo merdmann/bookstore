c81cc51f174e49638c2949812fab5c465b42d7ba
Michael Erdamann <michael.erdmann@snafu.de>
2019-12-11 10:40:41 +0100

    still fixing

--- js/main.js
+++ js/main.js
@@ -1,165 +1,117 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM Tree loaded')
+document.addEventListener('DOMContentLoaded', function() {
+        console.log('DOM Tree loaded')
 
-    var searchableContents = new Map;
+        var searchableContents = new Map;
 
-    function main() {
-        fetchData("https://api.myjson.com/bins/udbm5")
+        function main() {
+            fetchData("https://api.myjson.com/bins/udbm5")
 
-        console.log("Setting up search");
-        const btnBookSearch = document.getElementById("btn-book-search");
-        const searchString = document.getElementById("search-string");
+            console.log("Setting up search");
+            const btnBookSearch = document.getElementById("btn-book-search");
+            const searchString = document.getElementById("search-string");
 
-        searchString.addEventListener("keyup", function (event) {
-            // Number 13 is the "Enter" key on the keyboard
-            console.log("do search:" + searchString.value);
-            event.preventDefault();
-            const result = search(searchString.value);
-            result.forEach(function (book) {
-                Show('card', book)
-            })
-        })
-    }
-
-    main();
-
-    //
-    // find a key which contains the value and return the objet data
-    // lets hope that map handles laege keys better then i could
-    //
-    function search(value) {
-        let result = [];
-
-        console.log("search for " + value);
-
-        searchableContents.forEach(function (item, book) {
-            const key = Array.from(SearchKey(item));
-
-            if (key.indexOf(value) > 0)
-                result.push(book)
-        });
-
-        console.log(result);
-        return result;
-    }
-
-
-    //
-    // THis function is expected to simplify process of building dsynamically data
-    // sructures.
-    //
-    function addElement(parent, elem, className, id, value) {
-        var element = document.createElement(elem);
-
-        if (className !== "") {
-            var classes = className.split(" ");
-
-            if (classes.length > 0) {
-                classes.forEach(function (item) {
-                    element.classList.add(item)
+            searchString.addEventListener("keyup", function(event) {
+                // Number 13 is the "Enter" key on the keyboard
+                console.log("do search:" + searchString.value);
+                event.preventDefault();
+                const result = search(searchString.value);
+                result.forEach(function(book) {
+                    Show('card', book)
                 })
-            }
+            })
         }
 
-        if (id !== "")
-            element.setAttribute(id, value);
+        main();
 
-        parent.appendChild(element)
+        //
+        // find a key which contains the value and return the objet data
+        // lets hope that map handles laege keys better then i could
+        //
+        function search(value) {
+            let result = [];
 
-        return element;
-    }
+            console.log("search for " + value);
 
-    // create a unique id for a css object
-    function uniqId(item, id) {
-        return item + "-" + id;
-    }
+            searchableContents.forEach(function(item, book) {
+                const key = Array.from(SearchKey(item));
 
-    // place a card newr the given location
-    function placeCard(locationId) {
-        console.log("placeCard(" + locationId + ")");
-        const root = document.getElementById(locationId);
+                if (key.indexOf(value) > 0)
+                    result.push(book)
+            });
 
-        const div0 = addElement(root, "div", "", "id", "card");
-        div0.setAttribute("style", "width: 18rem;");
+            console.log(result);
+            return result;
+        }
 
-        // debuggign css added
-        const div1 = addElement(div0, "div", "flip-card book", "style", "border: 1px solid red;")
-        const div2 = addElement(div1, "div", "flip-card-inner", "", "")
-        const div3 = addElement(div2, "div", "flip-card-front", "", "")
-        const img = addElement(div3, "img", "", "id", uniqId(locationId, "book-front-cover"))
-        img.setAttribute("style", "width:80%");
-        const div4 = addElement(div3, "div", "flip-card-back", "");
-        const h1 = addElement(div3, "h3", "", "id", uniqId(locationId, "book-autor-name"));
-        const p = addElement(div3, "p", "", "", "");
-        const div5 = addElement(div0, "div", "card-body", "", "");
-        const h5 = addElement(div0, "h5", "card-title", "id", uniqId(locationId, "book-title"));
-        const pp = addElement(div0, "p", "card-text", "id", uniqId(locationId, "card-text"));
-        const a = addElement(div0, "a", "btn", "id", uniqId(locationId, "more-info"));
-        a.classList.add("btn-primary");
-        root.appendChild(div0);
 
-        return root;
-    }
 
-    function SearchKey(elem) {
-        return elem.descripcion & elem.titulo;
-    }
+        // create a unique id for a css object
+        function uniqId(item, id) {
+            return item + "-" + id;
+        }
 
-    function Show(location, elem) {
-        const frontCover = elem.portada;
-        const moreInfo = elem.detallee;
-        const title = elem.titolo;
+        // place a card newr the given location
+        function placeCard(locationId, elem) {
+            const root = document.getElementById(locationId);
+            const tile = `<div class="card text-center" style="width: 100px">
+                <img class="card-img-top imgs" src="${elem.portada}" alt=" cover ${elem.titulo} not availale">
+                <div class="card-img-overlay text-right text-light">
+                <h4 class="card-title">${elem.titulo}</div>
+                <p class="card-text">${elem.titulo}</p>
+                </div>
+                <div class="card-body">
+                   ${elem.titulo}
+                </div>
+                </div>`
+            root.innerHTML += tile;
+        }
 
-        //console.log( elem )
-        console.log("Show:" + elem.titulo);
-        searchableContents.set(SearchKey(elem), elem);
+        function SearchKey(elem) {
+            return elem.descripcion & elem.titulo;
+        }
 
-        placeCard(location);
+        function Show(location, elem) {
+            const frontCover = elem.portada;
+            const moreInfo = elem.detallee;
+            const title = elem.titolo;
 
-        const _book_front_cover_ = document.getElementById(uniqId(location, "book-front-cover"));
-        _book_front_cover_.setAttribute("src", frontCover); // put the image
+            console.log(elem)
+            console.log("Show:" + elem.titulo);
+            searchableContents.set(SearchKey(elem), elem);
 
-        //const _book_autor_name_ = document.getElementById(uniqId(location, "book-autor-name"));
-        //_book_autor_name_.innerHTML = elem.descripcion;
-        const _book_title_ = document.getElementById(uniqId(location, "book-title"));
-        _book_title_.innerHTML = elem.titulo;
-        const _more_info_ = document.getElementById(uniqId(location, "more-info"));
-        _more_info_.setAttribute("href", elem.detallee);
+            placeCard(location, elem);
+        };
 
-        //var _book_description_ = document.getElementById(uniqId( location, "book-description"));
-        //  _book_description_.innerHTML = elem.description;
-    };
+        function ProcessAndRender(data) {
+            const books = data["books"]
 
-    function ProcessAndRender(data) {
-        const books = data["books"]
-
-        books.forEach(function (item) {
-            Show("card", item);
-        })
-    }
-
-    // Needs to be refctored since it hides intention bhind a boolean,
-    function fetchData(url) {
-        fetch(url, {
-                headers: {
-                    'Content-Type': 'application/json'
-                    /* "X-API-Key": "wqmgqOHo1JMAkYIfh3sJr4FlUN3PCokyojEziJBK" */
-                }
-                /* mode: "cors" */
+            books.forEach(function(item) {
+                Show("card", item);
             })
-            .then(function (response) {
-                document.body.style.cursor = 'wait'
-                return response.json()
-            })
-            .then(function (myJson) {
-                //console.log(myJson);
-                document.body.style.cursor = 'auto'
+        }
 
-                ProcessAndRender(myJson)
-            })
-            .catch(err => console.log(err))
-    }
+        // Needs to be refctored since it hides intention bhind a boolean,
+        function fetchData(url) {
+            fetch(url, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                            /* "X-API-Key": "wqmgqOHo1JMAkYIfh3sJr4FlUN3PCokyojEziJBK" */
+                    }
+                    /* mode: "cors" */
+                })
+                .then(function(response) {
+                    document.body.style.cursor = 'wait'
+                    return response.json()
+                })
+                .then(function(myJson) {
+                    //console.log(myJson);
+                    document.body.style.cursor = 'auto'
 
-}) // DOMContentLoaded handler
+                    ProcessAndRender(myJson)
+                })
+                .catch(err => console.log(err))
+        }
+
+    }) // DOMContentLoaded handler
